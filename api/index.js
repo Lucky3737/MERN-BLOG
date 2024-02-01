3bb0ac700d971a7c169f3013f8bfb97e3039a303
chennurisagar <chennurisagar321@gmail.com>
2024-02-01 15:34:37 +0530

    create middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./routes/user.route.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -14,8 +15,23 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is rUnning the Port 3000");
 });
 
-app.use("/api/user", router);
+app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
