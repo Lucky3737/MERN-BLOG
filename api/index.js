27400f09b4c360f521ecea7d426aabd15dca89f7
chennurisagar <chennurisagar321@gmail.com>
2024-02-01 11:50:12 +0530

    create routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import router from "./routes/user.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,3 +17,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is rUnning the Port 3000");
 });
+
+app.use("/api/user", router);
