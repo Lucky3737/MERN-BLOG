0482fce22f4172b70d78ad11f18d576c7692ee27
chennurisagar <chennurisagar321@gmail.com>
2024-02-02 10:00:05 +0530

    add funtionality to signup

--- mern-client/src/pages/SignUp.jsx
+++ mern-client/src/pages/SignUp.jsx
@@ -1,3 +1,116 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Fragment, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please Fill All Fields");
+    }
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="p-4 flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-4">
+        {/*left tab */}
+        <div className="flex-1">
+          <Link to="/" className=" text-4xl font-bold dark:text-white">
+            <span className="px-1 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md text-white">
+              SARGARS
+            </span>
+            BLOG
+          </Link>
+          <p className="text-gray font-semibold mt-5">
+            This is aDemo Project, you can SignUp Gmail and Password
+          </p>
+        </div>
+        {/* right tab*/}
+        <div className="sm:mt-5 md:ml-10 flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="UserEmail"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="YourPassword"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              type="submit"
+              gradientDuoTone={"purpleToPink"}
+              disabled={loading}
+            >
+              {loading ? (
+                <Fragment>
+                  <Spinner size="sm" /> <span>Loading ...</span>
+                </Fragment>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-3 mt-5 text-sm ">
+            <span>Have Accout ? </span>
+            <Link to="/sign-in" className="text-blue-400">
+              Signin
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
